baf4143fc0b8cbd91c82100fcb391d6b9676d2d2
Marcus Thorne <marcus_thorne475@hotmail.com>
2023-05-14 11:06:48 +0100

    a couple more modules done

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
-const http = require('http');
+const EventEmitter = require('events');
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        res.end('Home Page')
-    }
+const customEmitter = new EventEmitter();
 
-    if(req.url === '/about') {
-        // Blocking code
-        for(let i = 0; i < 1000; i++) {
-            for(let j = 0; j < 1000; j++) {
-                console.log(`${i}, ${j}`);
-            }
-        }
-
-        res.end('About Page')
-    }
-
-    res.end('Error Page')
+customEmitter.on('response', () => {
+    console.log(`data recieved `);
 })
 
-server.listen(3000, () => {
-    console.log('Server is listening on port: 3000...');
-})
+customEmitter.emit('response')
